4717b1d4d745b7ef739abcfec5f7756a3284238f
uemue <7414847+uemue@users.noreply.github.com>
2022-09-25 15:25:27 +0900

    Solve ABC269 C

--- abc269/src/bin/c.rs
+++ abc269/src/bin/c.rs
@@ -7,11 +7,23 @@ use superslice::*;
 
 fn main() {
     input! {
-        a: i64,
-        b: i64,
-        c: i64,
-        d: i64,
+        mut n: usize
     }
 
-    println!("{}", a * d - b * c);
+    let mut mask = 1;
+    let mut results: Vec<usize> = vec![0];
+    println!("{}", 0);
+
+    for _ in 0..60 {
+        if n & mask > 0 {
+            let mut new_results: Vec<usize> = Vec::new();
+            for res in &results {
+                let new_res = mask | res;
+                println!("{}", new_res);
+                new_results.push(new_res);
+            }
+            results.append(&mut new_results);
+        }
+        mask <<= 1;
+    }
 }
