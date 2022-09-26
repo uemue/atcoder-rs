d2b2669a38dc666aba17043b92965853c83c5b1b
uemue <7414847+uemue@users.noreply.github.com>
2022-09-26 20:31:31 +0900

    Solve ABC269 D

--- abc269/src/bin/d.rs
+++ abc269/src/bin/d.rs
@@ -7,11 +7,47 @@ use superslice::*;
 
 fn main() {
     input! {
-        a: i64,
-        b: i64,
-        c: i64,
-        d: i64,
+        n: usize,
+        black: [(i64, i64); n]
     }
 
-    println!("{}", a * d - b * c);
+    let black: HashSet<(i64, i64)> = black.into_iter().collect();
+    let mut visited: HashSet<(i64, i64)> = HashSet::new();
+    let mut result = 0;
+
+    for cell in &black {
+        if visited.contains(&cell) {
+            continue;
+        } else {
+            result += 1;
+            dfs(cell, &black, &mut visited);
+        }
+    }
+
+    println!("{}", result);
+}
+
+fn dfs(cell: &(i64, i64), black: &HashSet<(i64, i64)>, visited: &mut HashSet<(i64, i64)>) {
+    if visited.contains(&cell) {
+        return;
+    }
+
+    let &(i, j) = cell;
+    visited.insert((i, j));
+
+    let next_cells = vec![
+        (i - 1, j - 1),
+        (i - 1, j),
+        (i, j - 1),
+        (i, j + 1),
+        (i + 1, j),
+        (i + 1, j + 1),
+    ];
+
+    for next_cell in next_cells {
+        if !black.contains(&next_cell) {
+            continue;
+        }
+        dfs(&next_cell, black, visited);
+    }
 }
