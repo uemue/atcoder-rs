7499454bdb35a5d5faf30bbf9650c447a375b9d1
uemue <7414847+uemue@users.noreply.github.com>
2022-09-27 12:33:13 +0900

    Solve ABC269 E

--- abc269/src/bin/e.rs
+++ abc269/src/bin/e.rs
@@ -3,15 +3,56 @@ use itertools::Itertools;
 use proconio::{input, marker::*};
 use std::cmp::*;
 use std::collections::*;
+use std::ptr::read;
 use superslice::*;
+use text_io::read;
 
 fn main() {
-    input! {
-        a: i64,
-        b: i64,
-        c: i64,
-        d: i64,
+    let n: usize = read!();
+
+    // detect X(row number)
+    let mut ok = n;
+    let mut ng = 0;
+
+    while abs(ok, ng) > 1 {
+        let mid = (ok + ng) / 2;
+        if ask(1, mid, 1, n) < mid {
+            ok = mid;
+        } else {
+            ng = mid;
+        }
     }
 
-    println!("{}", a * d - b * c);
+    let x = ok;
+
+    // detect Y(column number)
+    let mut ok = n;
+    let mut ng = 0;
+
+    while abs(ok, ng) > 1 {
+        let mid = (ok + ng) / 2;
+        if ask(1, n, 1, mid) < mid {
+            ok = mid;
+        } else {
+            ng = mid;
+        }
+    }
+
+    let y = ok;
+
+    println!("! {} {}", x, y);
+}
+
+fn ask(a: usize, b: usize, c: usize, d: usize) -> usize {
+    println!("? {} {} {} {}", a, b, c, d);
+    let res: usize = read!();
+    res
+}
+
+fn abs(a: usize, b: usize) -> usize {
+    if a > b {
+        a - b
+    } else {
+        b - a
+    }
 }
