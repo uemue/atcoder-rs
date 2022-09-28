39f1b87aa26181505a826ebb1e35d48a99a6fe45
uemue <7414847+uemue@users.noreply.github.com>
2022-09-28 21:32:23 +0900

    Solve ABC270 C

--- abc270/src/bin/c.rs
+++ abc270/src/bin/c.rs
@@ -7,11 +7,52 @@ use superslice::*;
 
 fn main() {
     input! {
-        a: i64,
-        b: i64,
-        c: i64,
-        d: i64,
+        n: usize,
+        x: Usize1,
+        y: Usize1,
+        edges: [(Usize1, Usize1); n-1]
     }
 
-    println!("{}", a * d - b * c);
+    let mut tree: Vec<Vec<usize>> = vec![vec![]; n];
+
+    for (from, to) in edges {
+        tree[from].push(to);
+        tree[to].push(from);
+    }
+
+    let mut seen: Vec<bool> = vec![false; n];
+    let mut route: Vec<usize> = vec![];
+
+    dfs(x, y, &tree, &mut seen, &mut route);
+    route.reverse();
+
+    print!("{}", x + 1);
+    for i in route {
+        print!(" {}", i + 1);
+    }
+}
+
+fn dfs(
+    start: usize,
+    goal: usize,
+    tree: &Vec<Vec<usize>>,
+    seen: &mut Vec<bool>,
+    route: &mut Vec<usize>,
+) -> bool {
+    seen[start] = true;
+    if start == goal {
+        return true;
+    }
+
+    for &next in &tree[start] {
+        if seen[next] {
+            continue;
+        }
+        let res = dfs(next, goal, tree, seen, route);
+        if res {
+            route.push(next);
+            return true;
+        }
+    }
+    return false;
 }
